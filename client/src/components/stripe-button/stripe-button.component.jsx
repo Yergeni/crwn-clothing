01a0df4021e819cd541e48ebcb934253478654af
Yergeni <yergenis@gmail.com>
2020-09-22 21:32:54 -0400

    Reestablish after messup

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+
+const StripeCheckoutButton = ({ price }) => {
+  // console.log('STRIPE KEY on dev env: ', process.env.REACT_APP_STRIPE_PUBLISH_KEY)
+  const priceForStripe = price * 100; // Stripe require price in cents
+  const publishableKey = process.env.REACT_APP_STRIPE_PUBLISH_KEY;
+  // const publishableKey = ''
+
+  const onToken = (token) => {
+    console.log(token); // token for making payment on stripe
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment successfull!");
+      })
+      .catch((error) => {
+        console.log("Pyament error: ", JSON.parse(error));
+        alert(
+          "There was an issue with the payment. Please sure you use the test credit card"
+        );
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken} // The token is the onSuccess when calling the submision for stripe
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+if (process.env.NODE_ENV !== 'production') {
+    // load dotenv module in environments other than production
+    require('dotenv').config();
+}
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY); // give back a function that its first param is the srecret key from stripe account
+
+const app = express();
+const port = process.env.PORT || 5000; // When it gets deployed to Heroku, it will set the process PORT 
+
+app.use(bodyParser.json()); // process al the incoming request bodies and covert them to json
+app.use(bodyParser.urlencoded({ extended: true })); // encode the url
+
+app.use(cors()); // allow cross-origin request
+
+// Serve all static files
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    // any url the user hits responds with the index.html file
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html')) // send our static files (HTML, JS, CSS)
+    });
+}
+
+// Start listening for request
+app.listen(port, error => {
+    if (error) throw error;
+    console.log('Server running on port: ' + port);
+})
+
+// Post route to payment
+app.post('/payment', (req, res) => {
+    // a token will come from the frontend on the request
+    const body = {
+        source: req.body.token.id,
+        amount: req.body.amount,
+        currency: 'usd'
+    };
+
+    stripe.charges.create(body, (stripeErr, stripeRes) => {
+        if(stripeErr) {
+            res.status(500).send({error: stripeErr});
+        } else {
+            res.status(200).send({success: stripeRes});
+        }
+    })
+})
