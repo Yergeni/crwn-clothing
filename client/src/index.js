6c6e81dcecc1bbc440997bd6428711ea6db8a898
Yergeni <yergenis@gmail.com>
2020-10-21 21:21:39 -0400

    updated our code to be PWA compliant, added serviceworker, https, updated manifest.json

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,9 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './redux/store';
 
+// Importing Service worker
+import * as serviceWorker from './serviceWorker'; 
+
 import './index.css';
 import App from './App';
 
@@ -19,3 +22,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const enforce = require('express-sslify'); // to enforce HTTP calls to use HTTPS
 
 if (process.env.NODE_ENV !== 'production') {
     // load dotenv module in environments other than production
@@ -15,6 +16,7 @@ const port = process.env.PORT || 5000; // When it gets deployed to Heroku, it wi
 
 app.use(bodyParser.json()); // process al the incoming request bodies and covert them to json
 app.use(bodyParser.urlencoded({ extended: true })); // encode the url
+app.use(enforce.HTTPS({ trustProtoHeader: true })); // trustProtoHeader needed for reverse proxy in Heroku
 
 app.use(cors()); // allow cross-origin request
 
@@ -32,7 +34,12 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, error => {
     if (error) throw error;
     console.log('Server running on port: ' + port);
-})
+});
+
+// Giving the service worker
+app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
 
 // Post route to payment
 app.post('/payment', (req, res) => {
